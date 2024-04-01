40036fcdf95b6b467868fb19cff9bed15d420dcb
ADIL-SALAH <122431444+ADIL-SALAH@users.noreply.github.com>
2024-04-01 17:32:10 +0530

    REDUX SETUP-PERSIST AND BROWSER TITLE ADDED

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Banner({ image, title, text }) {
     return (
-        <div className="h-screen w-screen bg-cover" style={{ backgroundImage: `url(/home/banner.jpg)` }}>
+        <div className="h-full w-full bg-cover" style={{ backgroundImage: `url(/home/banner.jpg)` }}>
             <div className="bg-black w-full h-full opacity-50 flex items-center p-10">
-                <div className="w-1/2">
-                    <h1 className="text-white md:text-5xl font-bold">{title}</h1>
-                    <p className="text-white">{text}</p>
+                <div className="lg:w-1/2 space-y-10">
+                    <h1 className="text-white md:text-5xl text-xl  font-bold">{title}</h1>
+                    <p className="text-white md:text-lg text-sm">{text}</p>
                 </div>
             </div>
         </div>
